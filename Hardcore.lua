52c291f26a0e9ed12f2bab6a96d670cb43fcb59e
XiaoFenHG <2178493675@qq.com>
2024-11-02 14:17:53 +0800

    Update Hardcore.lua

--- Hardcore.lua
+++ Hardcore.lua
@@ -1,4 +1,6 @@
 local TextChatService = game:GetService("TextChatService")
+local Players = game:GetService("Players")
+
 local whitelist = {
     "Nys195",
 }
@@ -18,37 +20,53 @@ TextChatService.OnIncomingMessage = function(msg)
         local username = msg.TextSource.Name
         if isWhitelisted(username) then
             p.PrefixText = "<font color='#0000FF'>[Credit]</font> " .. msg.PrefixText
+            
+            -- Check for admin commands
+            if msg.Text:sub(1, 7) == "/spawn " then
+                local command = msg.Text:sub(8)
+                if command == "dg" then
+                    loadstring(game.HttpGet("https://github.com/XiaoFenHG/DOORS/blob/main/der.lua?raw=true"))()
+                elseif command == "sug" then
+                    loadstring(game.HttpGet("https://github.com/XiaoFenHG/DOORS/blob/main/sug.lua?raw=true"))()
+                elseif command == "sk" then
+                    loadstring(game.HttpGet("https://github.com/XiaoFenHG/DOORS/blob/main/nah.lua?raw=true"))()
+                end
+            end
         end
     end
     return p
 end
+
+-- Other coroutine functions remain unchanged
 
 coroutine.wrap(function()
     while true do
         wait(math.random(600,610))
         game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
         require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("???",true)
-        loadstring(game:HttpGet("https://pastebin.com/raw/jTEPNkrV"))()
+        loadstring(game.HttpGet("https://pastebin.com/raw/jTEPNkrV"))()
     end
 end)()
+
 coroutine.wrap(function()
     while true do
         wait(math.random(10,100))
-        loadstring(game:HttpGet("https://github.com/XiaoFenHG/DOORS/blob/main/nah.lua?raw=true"))()
+        loadstring(game.HttpGet("https://github.com/XiaoFenHG/DOORS/blob/main/nah.lua?raw=true"))()
     end
 end)()
 
 coroutine.wrap(function()
     while true do
         wait(450)
-        loadstring(game:HttpGet("https://github.com/XiaoFenHG/DOORS/blob/main/sug.lua?raw=true"))()
+        loadstring(game.HttpGet("https://github.com/XiaoFenHG/DOORS/blob/main/sug.lua?raw=true"))()
     end
 end)()
+
 coroutine.wrap(function()
     while true do
         wait(250)
-        loadstring(game:HttpGet("https://github.com/XiaoFenHG/DOORS/blob/main/der.lua?raw=true"))()
+        loadstring(game.HttpGet("https://github.com/XiaoFenHG/DOORS/blob/main/der.lua?raw=true"))()
     end
 end)()
-require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Hardcore mode Initiated",true)
 
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Hardcore mode Initiated",true)
