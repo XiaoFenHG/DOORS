61bc1ed0a6f9944e68ae87302d24ea282c64a7df
FFF <2178493675@qq.com>
2024-12-14 12:41:55 +0800

    Update CustomSound.lua

--- CustomSound.lua
+++ CustomSound.lua
@@ -1,4 +1,3 @@
--- 定义函数来获取自定义音效ID
 local function GetGitSoundID(GithubSnd, SoundName)
     SoundName = tostring(SoundName)
     local url = GithubSnd
@@ -9,4 +8,22 @@ end
 
 local SelfModules = {
     Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))(),
+}
+
+local function PlayCustomSound(GithubSnd, SoundName, Volume, shouldPlay)
+    local SoundId = GetGitSoundID(GithubSnd, SoundName)
+    local soundInstance = Instance.new("Sound")
+    soundInstance.SoundId = SoundId
+    soundInstance.Volume = Volume or 1
+    soundInstance.Parent = workspace
+    if shouldPlay then
+        soundInstance:Play()
+    end
+    return soundInstance
+end
+
+return {
+    GetGitSoundID = GetGitSoundID,
+    PlayCustomSound = PlayCustomSound,
+    SelfModules = SelfModules
 }
